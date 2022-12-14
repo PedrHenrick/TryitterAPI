542749ad19d601143dbb2843836171d2da036c4b
Pedro Henrick <pedro.mesquita.hc@gmail.com>
2022-12-14 20:04:13 -0300

    feat(swagger) :sparkles: Adicionando configurações no swagger

--- TryitterWebAPI/Program.cs
+++ TryitterWebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TryitterWebAPI.Repository;
 using TryitterWebAPI.Repository;
 
@@ -6,13 +7,57 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TryitterContext>(optionsAction: options
     => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<StudentRepository>();
-builder.Services.AddScoped<PostRepository>();
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+
 
 builder.Services.AddScoped<ITryitterContext, TryitterContext>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Tryitter API",
+        Description = "An ASP.NET Core Web API for managing posts",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Pedro Henrick",
+            Url = new Uri("https://www.linkedin.com/in/pedro-henrick/")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Header de autorização JWT usando o esquema Bearer.\r\n\r\nInforme 'Bearer' + [espaço] + o seu token.\r\n\r\nExemplo: 'Bearer 12345abcdef'",
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+});
 
 var app = builder.Build();
 
