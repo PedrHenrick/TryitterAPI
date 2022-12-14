be33935565f86b8b0257c9c274fc54cad99caeb4
Pedro Henrick <pedro.mesquita.hc@gmail.com>
2022-12-14 19:48:33 -0300

    feat(controller) :sparkles: Adicionando controllers para as rotas post e student

---
+++ TryitterWebAPI/Controllers/PostController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TryitterAPI.Models;
+using TryitterWebAPI.Repository;
+
+namespace TryitterWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly PostRepository _repository;
+
+        public PostController(PostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Post post)
+        {
+            if (post == null)
+            {
+                return BadRequest();
+            }
+            _repository.CreatePost(post);
+            return CreatedAtAction("Get", new { id = post.PostId }, post);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Post post)
+        {
+            if (post == null || post.PostId != id)
+            {
+                return BadRequest();
+            }
+            var postInDb = _repository.GetPostById(id);
+            if (postInDb == null)
+            {
+                return NotFound();
+            }
+            _repository.UpdatePost(post, id);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var postInDb = _repository.GetPostById(id);
+            if (postInDb == null)
+            {
+                return NotFound();
+            }
+            _repository.DeletePost(id);
+            return NoContent();
+        }
+    }
+}
---
+++ TryitterWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TryitterAPI.Models;
+using TryitterAPI.Repository;
+
+namespace TryitterWebAPI.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly StudentRepository _repository;
+
+        public StudentController(StudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.GetStudents());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var student = _repository.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            if (student == null)
+            {
+                return BadRequest();
+            }
+            _repository.CreateStudent(student);
+            return CreatedAtAction("Get", new { id = student.StudentId }, student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Student student)
+        {
+            if (student == null || student.StudentId != id) BadRequest();
+
+            var studentInDb = _repository.GetStudentById(id);
+            
+            if (studentInDb == null) NotFound();
+
+            _repository.UpdateStudent(student, id);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var studentInDb = _repository.GetStudentById(id);
+            if (studentInDb == null) NotFound();
+     
+            _repository.DeleteStudent(id);
+            return NoContent();
+        }
+    }
+}
