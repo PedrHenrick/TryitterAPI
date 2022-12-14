c1b8699f97b8ab214a619c1eaa3015425afd1f0e
Pedro Henrick <pedro.mesquita.hc@gmail.com>
2022-12-14 19:29:53 -0300

    feat(config) :sparkles: Adicionando configurações de build

--- TryitterWebAPI/Program.cs
+++ TryitterWebAPI/Program.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TryitterAPI.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TryitterContext>(optionsAction: options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ITryitterContext, TryitterContext>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
